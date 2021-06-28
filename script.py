2f72567306da88b7e5612cc31598de0c9ce90f9b
AMZ <80452444+amegh-amz@users.noreply.github.com>
2021-06-28 14:12:45 +0530

    Update script.py

--- script.py
+++ script.py
@@ -53,7 +53,7 @@ No need add each filter again!
 Bot will automatically search for your files and give links to that!
 
 
-<b>© @amzmtaccount</b>"""
+<b>© @Blind_psycho_TG</b>"""
 
 
     ABOUT_MSG = """⭕️<b>My Name : Auto Filter Bot V2</b>
